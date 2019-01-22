bede1eeacd5d2e0b12da2d9744c1bf1c0f705736
Seiji <seiji.okamoto@thirdbridge.com>
2019-01-22 11:46:41 +0000

    Fix version linking syntax

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cp -r /src /go/src/github.com/gliderlabs/resolvable
 cd /go/src/github.com/gliderlabs/resolvable
 export GOPATH=/go
 go get
-go build -ldflags "-X main.Version $1" -o /bin/resolvable
+go build -ldflags "-X main.Version=$1" -o /bin/resolvable
 apk del --purge build-deps
 rm -rf /go
 rm -rf /var/cache/apk/*
